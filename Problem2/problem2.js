3bee998c8aedf564e544b9496b8cfccf96853633
ebal5 <eval.scheme@gmail.com>
2022-06-28 04:48:55 +0900

    速度改善

--- Problem2/problem2.js
+++ Problem2/problem2.js
@@ -8,18 +8,41 @@ function main(input) {
 }
 
 function test(_a, _b, _c) {
-  const queue = [[_a, _b, _c, 0]];
+  if (valid(_a, _b, _c)) return 0;
+  const queue = [];
+  const max = Math.max(_a, _b, _c);
+  const _second = second(_a, _b, _c);
+  if(max - _second > 2) {
+    const level = max - (_second + 1);
+    if (max === _a) {
+      queue.push([_a - level, _b, _c, level]);
+    } else if (max === _b) {
+      if (_a == _c) {
+        if (_a >= 2) return 1;
+      } else {
+        queue.push([_a, _b - level, _c, level]);
+      }
+    } else {
+      queue.push([_a, _b, _c - level, level]);
+    }
+  } else {
+    if (_a - 1 > 0) queue.push([_a - 1, _b, _c, 1]);
+    if (_b - 1 > 0) queue.push([_a, _b - 1, _c, 1]);
+    if (_c - 1 > 0) queue.push([_a, _b, _c - 1, 1]);
+  }
   while (queue.length != 0) {
     const [a, b, c, level] = queue.shift();
-    if (valid(a, b, c)) return level;
+    if (valid(a, b, c)) {
+      return level;
+    }
     if (a - 1 > 0) {
-      queue.push([a-1, b, c, level + 1]);
+      queue.push([a - 1, b, c, level + 1]);
     }
     if (b - 1 > 0) {
-      queue.push([a, b-1, c, level + 1]);
+      queue.push([a, b - 1, c, level + 1]);
     }
-    if ( c - 1 > 0) {
-      queue.push([a, b, c-1, level + 1]);
+    if (c - 1 > 0) {
+      queue.push([a, b, c - 1, level + 1]);
     }
   }
   return -1;
@@ -31,8 +54,8 @@ function valid(a, b, c) {
 }
 
 function second(...arr) {
-  arr.sort();
-  return arr[1];
+  const sorted = arr.sort((x, y) => x - y).reverse();
+  return sorted[1];
 }
 
 var input = '';
